d5bf054b77b02f8541f05bc11f5a1e6ec4fbc33a
Your Name <you@example.com>
2021-01-08 10:58:50 -0300

    primeiro commit

---
+++ components/home/index.js
@@ -0,0 +1,11 @@
+//import "./styles.css"
+
+function Home() {
+    return(
+        <div className='home'>
+            <h1>Meu Blog pessoal sobre RH</h1>
+        </div>
+    )
+}
+
+export default Home;
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Home from '../components/home';
+import Footer from '../components/footer'
+
+function Template() {
+    return(
+       <div>
+            <Home></Home>
+            <Footer></Footer>
+       </div>
+    )
+}
+
+export default Template;
